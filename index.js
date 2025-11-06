b3c2cc9d670d4392bcae3973ff638e88e77085f2
Honeydei Yssabelle Baran Nakagawa <hybnakagawa@addunet.ph>
2025-11-06 20:28:19 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const mysql = require('mysql');
+
+app.use(express.json());
+
+
+  const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "itelective"
+  });
+  db.getConnection((err, connection) => {
+    if (err) {
+      console.error("Error connecting to the database:", err);
+      return;
+    }
+    console.log("Connected to the database");
+    connection.release();
+  });
+
+
+
+
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
+
+
+
+app.get('/karaage', async (req, res) => {
+    try {
+        const myquery = "SELECT * from meal;"
+        db.query(myquery, (err, result) => {
+            if (err) {
+                console.log("sql error:", err);
+            }
+            else{
+                res.send(result)
+            }
+        });
+        const response = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=adobo');
+        const data = await response.json();
+        res.json(data);
+    }
+    catch(error){
+        console.error(error)
+        res.status(500).json({error: "Recipe not found"})
+    }
+});
+
+app.post('/insertmeal', (req, res) => {
+
+    const {mealname, mealcategory, mealarea, mealinstructions} = req.body;
+    const myquery = "INSERT INTO meal (meal, category, area, instructions) VALUES (?,?,?,?);"
+    db.query(myquery, [mealname, mealcategory, mealarea, mealinstructions], (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+              console.log(result);
+        }
+    });
+});
+
+app.listen(8000, () => {
+  console.log('Server is running on port 8000');
+});
